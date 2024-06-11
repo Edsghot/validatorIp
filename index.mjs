6b3e6ad445b4fb6a608b39593af7de956a43aec6
edsghot <edsghot@gmail.com>
2024-06-11 13:15:04 -0500

    agregando para que muestre mas propiedades

--- index.mjs
+++ index.mjs
@@ -27,6 +27,19 @@ function obtenerMascaraSubred(claseIP) {
     }
 }
 
+function obtenerMascaraCIDR(claseIP) {
+    switch (claseIP) {
+        case 'A':
+            return '/8';
+        case 'B':
+            return '/16';
+        case 'C':
+            return '/24';
+        default:
+            return 'Desconocida';
+    }
+}
+
 function obtenerIDRed(ip, claseIP) {
     const octetos = ip.split('.');
     switch (claseIP) {
@@ -41,6 +54,48 @@ function obtenerIDRed(ip, claseIP) {
     }
 }
 
+function obtenerIPRed(ip, claseIP) {
+    const octetos = ip.split('.');
+    switch (claseIP) {
+        case 'A':
+            return `${octetos[0]}.0.0.0`;
+        case 'B':
+            return `${octetos[0]}.${octetos[1]}.0.0`;
+        case 'C':
+            return `${octetos[0]}.${octetos[1]}.${octetos[2]}.0`;
+        default:
+            return 'Desconocida';
+    }
+}
+
+function obtenerIPHost(ip, claseIP) {
+    const octetos = ip.split('.');
+    switch (claseIP) {
+        case 'A':
+            return `0.${octetos[1]}.${octetos[2]}.${octetos[3]}`;
+        case 'B':
+            return `0.0.${octetos[2]}.${octetos[3]}`;
+        case 'C':
+            return `0.0.0.${octetos[3]}`;
+        default:
+            return 'Desconocida';
+    }
+}
+
+function obtenerIDHost(ip, claseIP) {
+    const octetos = ip.split('.');
+    switch (claseIP) {
+        case 'A':
+            return `${octetos[1]}.${octetos[2]}.${octetos[3]}`;
+        case 'B':
+            return `${octetos[2]}.${octetos[3]}`;
+        case 'C':
+            return `${octetos[3]}`;
+        default:
+            return 'Desconocido';
+    }
+}
+
 function esIPValida(ip) {
     return net.isIP(ip);
 }
@@ -50,30 +105,44 @@ function main(ip) {
     console.log("===============================================");
     console.log("====Nombre: Ed soto huamanhorcco  =============");
     console.log("===============================================");
-    console.log("==========INFO: "+ip+"=======");
+    console.log("==========INFO: " + ip + "=======");
     console.log("===============================================");
     console.log(" ");
-    
 
     if (esIPValida(ip)) {
         const claseIP = obtenerClaseIP(ip);
         console.log(`CLASE: ${claseIP}`);
         console.log("---------------------");
+
         const mascaraSubred = obtenerMascaraSubred(claseIP);
-        console.log(`Máscara: ${claseIP}: ${mascaraSubred}`);
-        console.log("---------------------")
-        
+        console.log(`Máscara: ${mascaraSubred}`);
+        console.log("---------------------");
+
+        const mascaraCIDR = obtenerMascaraCIDR(claseIP);
+        console.log(`Máscara: ${mascaraCIDR}`);
+        console.log("---------------------");
+
         const idRed = obtenerIDRed(ip, claseIP);
-    
-        console.log(`ID de la red: ${claseIP}: ${idRed}`);
-        console.log("-----------------------")
+        console.log(`ID de la red: ${idRed}`);
+        console.log("---------------------");
+
+        const ipRed = obtenerIPRed(ip, claseIP);
+        console.log(`IP de la red: ${ipRed}`);
+        console.log("---------------------");
+
+        const ipHost = obtenerIPHost(ip, claseIP);
+        console.log(`IP del host: ${ipHost}`);
+        console.log("---------------------");
+
+        const idHost = obtenerIDHost(ip, claseIP);
+        console.log(`ID del host: ${idHost}`);
+        console.log("---------------------");
     } else {
         console.error('La entrada proporcionada no es una dirección IP válida.');
-        
-console.log("")
-console.log("")
-
     }
+
+    console.log("");
+    console.log("");
 }
 
 const rl = readline.createInterface({
@@ -81,7 +150,7 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-console.log("")
+console.log("");
 rl.question('Por favor, introduce una dirección IP: ', (input) => {
     main(input);
     rl.close();
